835a4b3ca481328f88a9935bad160bf741caafea
Rthomps91011 <beccafisher123@gmail.com>
2018-08-20 23:22:01 -0400

    Initial commit

---
+++ Clockwork.API/Controllers/CurrentTimeController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+
+namespace Clockwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class CurrentTimeController : Controller
+    {
+        // GET api/currenttime
+        [HttpGet()]
+        public IActionResult Get([FromQuery]string timezone)
+        {
+            Console.WriteLine("our timezone was: " + timezone);
+            var utcTime = DateTime.UtcNow;
+            var localTime = DateTime.Now;
+            if (timezone != null)
+            {
+                try
+                {
+                    TimeZoneInfo localZone = TimeZoneInfo.FindSystemTimeZoneById(timezone);
+                    localTime = TimeZoneInfo.ConvertTimeFromUtc(utcTime, localZone);
+                }
+                catch(Exception e)
+                {
+                    return StatusCode(400);
+                }
+            }
+            
+        var ip = this.HttpContext.Connection.RemoteIpAddress.ToString();
+
+            var returnVal = new CurrentTimeQuery
+            {
+                UTCTime = utcTime,
+                ClientIp = ip,
+                Time = localTime,
+                
+             
+            };
+
+            using (var db = new ClockworkContext())
+            {
+
+                db.CurrentTimeQueries.Add(returnVal);
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                foreach (var CurrentTimeQuery in db.CurrentTimeQueries)
+                {
+                    Console.WriteLine(" - {0}", CurrentTimeQuery);
+
+                    
+                }
+            }
+
+            return Ok(returnVal);
+        }
+    }
+}
---
+++ Clockwork.API/Controllers/LogDatabaseController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+using Clockwork.API.Models;
+using System.Collections.Generic;
+
+namespace Clockwork.API.Controllers
+{
+    [Route("api/[controller]")]
+    public class LogDatabaseController : Controller
+    {
+        // GET api/currenttime
+        [HttpGet]
+        public IActionResult Get()
+        {
+
+
+
+            using (var db = new ClockworkContext())
+            {
+
+                List<CurrentTimeQuery> logs = new List<CurrentTimeQuery>();
+
+                foreach (var log in db.CurrentTimeQueries)
+                {
+                    logs.Add(log);
+                }
+
+                return Ok(logs);
+            }
+        }
+    }
+}
+
